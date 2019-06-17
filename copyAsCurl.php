bc170f0cfb6086c75161e8b05b45da46b8a5fd70
YWB <1540601646@qq.com>
2019-06-17 16:55:11 +0800

    新增文件实现copy as curl

---
+++ copyAsCurl.php
@@ -0,0 +1,60 @@
+<?php
+
+/**
+ * 该方法可以实现类似于谷歌浏览器的copy -> copy as curl(cmd)功能
+ * User: YWB
+ * Date: 2019/6/17 0017
+ * Time: 16:52
+ * @return string
+ */
+
+function getCurlCommand() {
+    try {
+        if (php_sapi_name() == 'error cli') {
+            throw new Exception("cli");
+        }
+
+        $curl_command = 'curl ';
+        $post_data = $get_data = '';
+
+        if($_GET) {
+            $gets = http_build_query($_GET);
+            $get_data .= strpos($curl_command, '?') ? '&' . $gets : '?' . $gets;
+        }
+
+        if ($_SERVER['REQUEST_METHOD'] == 'POST') {
+            $posts = http_build_query($_POST);
+            $post_data = ' -d "' . $posts . '"';
+        }
+
+        $path = isset($_SERVER['SCRIPT_NAME']) ? $_SERVER['SCRIPT_NAME'] : $_SERVER['PHP_SELF'];
+        $curl_command .= '"' . "http://{$_SERVER['HTTP_HOST']}" . $path . $get_data . '"';
+        if ($post_data) {
+            $curl_command .= $post_data;
+        }
+
+        $headers = array();
+        if (function_exists('getallheaders')) {
+            $headers = getallheaders();
+        } else {
+            foreach ($_SERVER as $name => $value) {
+                if (substr($name, 0, 5) == 'HTTP_') {
+                    $headers[str_replace(' ', '-', ucwords(strtolower(str_replace('_', ' ', substr($name, 5)))))] = $value;
+                }
+            }
+        }
+        foreach ($headers as $key => $value) {
+            if($key == 'Accept-Encoding')  $value = str_replace('gzip, ', '', $value);
+            $curl_command .= ' -H "' . $key . ':' . $value . '"';
+        }
+
+        return $curl_command;
+    } catch (Exception $e) {
+        return $e->getMessage();
+    }
+
+}
+
+echo getCurlCommand();
+
+
